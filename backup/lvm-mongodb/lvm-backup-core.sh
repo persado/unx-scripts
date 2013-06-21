e918b326a2de8d37315065c866a999bee50cf4fe
Thanos Angelatos <thanos.angelatos@upstreamsystems.com>
2013-06-21 20:18:05 +0300

    auto-update added

--- backup/lvm-mongodb/lvm-backup-core.sh
+++ backup/lvm-mongodb/lvm-backup-core.sh
@@ -45,6 +45,39 @@
 # is still alive as an indication that the script is still working.
 #
 
+##
+## auto-update: if the updated is different, we will execute that instead.
+##
+wget "https://raw.github.com/persado/unx-scripts/master/backup/lvm-mongodb/lvm-backup-core.sh"
+if [ ! -f "lvm-backup-core.sh" ]; 
+then
+	echo "WARNING: auto-update has failed, check location!!!"
+else
+	if diff lvm-backup-core.sh lvsbackup-data.sh >/dev/null ; then
+		echo "auto-update: no update is necessary"
+	else 
+		echo "auto-update: new version found!"
+		mv lvm-backup-core.sh lvsbackup-data.sh
+		chmod +x lvsbackup-data.sh
+		exec lvsbackup-data.sh
+		exit 0
+	fi
+fi
+
+
+##
+## warning: the following bit will check the existence of an older version and migrate the
+## configuration there. It should only happen once.
+if [ ! -f "~/.migrated-lvm-backup" ]; # migration is needed
+then
+	sed -e '1,/CONFIGURATION/d' lvsbackup-data.sh | sed -e '/^fi$/,$d' - > config
+	echo "fi" >> config
+	mv config lvm-backup-config
+	touch ~/.migrated-lvm-backup
+	echo "$0 migrated config data"
+	cp $0 lvsbackup-data.sh
+	echo "migrated ourselves to proper location
+fi
 #
 # ****** CONFIGURATION
 #
