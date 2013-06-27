7897503b34af5404288be5cdf08bf26076628850
Thanos Angelatos <thanos.angelatos@upstreamsystems.com>
2013-06-28 00:40:31 +0300

    fixed copyright and license

--- backup/lvm-mongodb/lvm-backup-core.sh
+++ backup/lvm-mongodb/lvm-backup-core.sh
@@ -4,7 +4,7 @@
 #
 # LICENSE
 #
-# Copyright (c) 2013, Thanos Angelatos agelatos@gmail.com
+# Copyright (c) 2013, Persado SA (persado.com) & A. Angelatos 
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -14,14 +14,14 @@
 #    * Redistributions in binary form must reproduce the above copyright
 #      notice, this list of conditions and the following disclaimer in the
 #      documentation and/or other materials provided with the distribution.
-#    * Neither the name of the Thanos Angelatos nor the
+#    * Neither the name of Persado SA or A. Angelatos nor the
 #      names of its contributors may be used to endorse or promote products
 #      derived from this software without specific prior written permission.
 #
 #THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 #ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 #WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-#DISCLAIMED. IN NO EVENT SHALL Thanos Angelatos BE LIABLE FOR ANY
+#DISCLAIMED. IN NO EVENT SHALL Persado SA or A. Angelatos BE LIABLE FOR ANY
 #DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 #(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
 #LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
