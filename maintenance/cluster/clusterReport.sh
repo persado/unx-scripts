e7395338fa7f07a7af07660f275ee0fd6fd47bd9
Thanos Angelatos <thanos.angelatos@upstreamsystems.com>
2013-07-25 14:28:57 +0300

    added primary detection and list of servers that can be compacted

--- maintenance/cluster/clusterReport.sh
+++ maintenance/cluster/clusterReport.sh
@@ -17,24 +17,62 @@ function diskSpace() {
 
    if [[ $used > $PCTFAIL ]] ;
    then
-      echo " ****FAILURE IMMINENT**** CANNOT COMPACT! " >> $1.log
+      echo -n " ****FAILURE IMMINENT**** CANNOT COMPACT AUTOMATICALLY! " >> $1.log
+      CAN_COMPACT=0
    elif [[ $used > $PCTWARN ]] ; 
    then 
-      echo " WARNING " >> $1.log
+      echo -n " WARNING " >> $1.log
+      CAN_COMPACT=1
    else
-      echo " " >> $1.log
+      echo -n " " >> $1.log
+      CAN_COMPACT=1
    fi   
 
-   tail -1 $1.log
+   tail -1 $1.log && echo ""
+}
+
+function isPrimary() {
+   echo -n "!" >> $1.log
+   ssh mongo@$1 \
+     "/opt/mongodb/bin/mongo localhost:27017/admin --quiet --eval 'rs.isMaster().secondary'  " > .tmp 2>/dev/null
+   secondary=`cat .tmp`
+   if [ $secondary == "true" ] ;
+   then 
+      PRIMARY=0
+      echo -n " [SECONDARY] " >> $1.log
+      echo -n "$1 is SECONDARY - "
+      if [[ $CAN_COMPACT > 0 ]] ; 
+      then 
+         echo -n "CAN BE AUTO-COMPACTED"
+      else 
+         echo -n "COMPACT NOT POSSIBLE"
+      fi
+   else
+      PRIMARY=1
+      echo " [PRIMARY] " >> $1.log
+   fi
+}
+
+function compactSecondary() {
+   if [[ $CAN_COMPACT > 0 && $PRIMARY < 1 ]] ;
+   then
+      echo " ---> adding $1 to compact configuration"
+      echo "$1" >> compact.cfg
+   fi
 }
 
 
-
-
-
+rm -f compact.cfg
 for p in $(cat hosts.cfg) 
 do
    rm -f $p.log>/dev/null
    diskSpace $p data
+   isPrimary $p
+   compactSecondary $p
+   echo "" >> $p.log 
 done 
 
+
+echo "-----------------------------------------" 
+echo "the following nodes need to be compacted:"
+cat compact.cfg
