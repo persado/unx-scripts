d01529681c377248ede748f1d156852ff0e8aa89
Thanos Angelatos <thanos.angelatos@persado.com>
2013-10-30 16:35:23 +0200

    updated with parameters

--- maintenance/activemq/README.md
+++ maintenance/activemq/README.md
@@ -3,9 +3,16 @@ Purge script
 
 This script connects to the localhost ActiveMQ admin interface and checks the size of the queue "DLQ.inbound". With some customisation it can monitor/check any other queue. It will purge the queue using the admin interface if it exceeds 50000 (0.5M) messages.
 
-It should be installed in the ActiveMQ users' crontab with an entry similar to this one:
-
+It should be installed in the ActiveMQ users' crontab with an entry similar to this one for default settings:
 
 ```
 */10 * * * * cd /home/activemq/purgeDLQ && ./purge.sh > purge.log 2> /dev/null
 ```
+
+for different queues, you need additional entries like the one below. It will purge if queue mcs.outbound.smsmt.claro_com_br.DLQ goes over 20000 messages.
+
+```
+*/10 * * * * cd /home/activemq/purgeDLQ && ./purge.sh mcs.outbound.smsmt.claro_com_br.DLQ 20000 > purge.log 2> /dev/null
+```
+
+
--- maintenance/activemq/purge.sh
+++ maintenance/activemq/purge.sh
@@ -1,16 +1,46 @@
 #!/bin/bash
-AMQ_BIN=/opt/activemq/bin
-ACTIVEMQ_HOME=/opt/activemq
-ACTIVEMQ_BASE=$ACTIVEMQ_HOME
+#
+# AMQ Host
 AHOST="http://localhost:8161/admin"
+# Default Queue
+QUEUE="DLQ.inbound"
+# Limit - if exceeded, purge
+COUNT=500000
 
-PRGURL=$( curl $AHOST/queues.jsp|grep DLQ.inbound|grep purge| awk 'BEGIN { FS = "\"" }; {print $2}' )
-DLQ=$( curl $AHOST/xml/queues.jsp |grep -A 2 name=\"DLQ.inbound|tail -1|awk 'BEGIN { FS = "\"" }; {print $2}' )
-PURL="$AHOST/$PRGURL"
-echo "`date` DLQ Found: $DLQ"
-echo "Purge URL = $PURL"
-if [ $DLQ -gt 500000 ];
+# first argument is the queue name
+if [ ! -z "$1" ]; 
 then
-	echo "Will now ask to purge DLQ"
-	curl "$PURL"
+	QUEUE="$1"
+fi
+# second argument is the limit
+if [ ! -z "$2" ];
+then	
+	COUNT=$2
+fi
+
+echo "AMQ Queue Purge"
+echo "Usage: $0 [Queue to monitor] [limit to start purging]"
+echo " "
+echo -n "Working ."
+PRGURL=$( curl $AHOST/queues.jsp 2>/dev/null |grep $QUEUE |grep purge| awk 'BEGIN { FS = "\"" }; {print $2}' )
+echo -n ".."
+MSGS=$( curl $AHOST/xml/queues.jsp 2>/dev/null |grep -A 2 name=\"$QUEUE |tail -1|awk 'BEGIN { FS = "\"" }; {print $2}' )
+echo ".."
+PURL="$AHOST/$PRGURL"
+
+echo "QUEUE = $QUEUE"
+echo "LIMIT = $COUNT"
+if [ "x" != "x$MSGS" ]; 
+then
+	echo "FOUND = $MSGS"
+	if [ $MSGS -gt $COUNT ];
+	then
+		echo "Found $MSGS messages over the limit ($COUNT) : now asking to purge $QUEUE - please wait...."
+		curl "$PURL"
+		echo "$QUEUE should now be empty."
+	else
+		echo "$QUEUE has $MSGS messages. Not purging."
+	fi
+else
+	echo "No messages found, connectivity/network error? ActiveMQ should be alive in address $AHOST"
 fi
